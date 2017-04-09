df09a0f9aefc1b25b5d845be774bf4d3131b9426
Felipe <felipe.osorio72@outlook.com.br>
2017-04-09 15:49:48 -0300

    Initializing base graph

--- src/Graph.hs
+++ src/Graph.hs
@@ -1,3 +1,5 @@
+infinity = 1e1000
+
 data Node = Node {
      value :: Int
      }
@@ -11,3 +13,16 @@ data Vertex = Vertex {
 data Graph = Graph [Vertex]
             deriving(Show)
 
+createNodes 0 = []
+createNodes num = (Node num) : (createNodes (num - 1))
+
+nodes = createNodes 5
+
+vertexes = [Vertex (nodes !! 0, nodes !! 1) 1, Vertex ( nodes !! 1, nodes !! 0) 1,
+		 	Vertex (nodes !! 0, nodes !! 2) 1, Vertex (nodes !! 2, nodes !! 0) 1,
+			Vertex (nodes !! 1, nodes !! 2) 13, Vertex ( nodes !! 2, nodes !! 1) 13,
+			Vertex (nodes !! 1, nodes !! 4) 2, Vertex ( nodes !! 4, nodes !! 1) 2,
+			Vertex (nodes !! 4, nodes !! 3) 8, Vertex ( nodes !! 3, nodes !! 4) 8,
+			Vertex (nodes !! 2, nodes !! 3) 3, Vertex (nodes !! 3, nodes !! 2) 3]
+
+g = Graph vertexes
