614518880abe4ad9b28cc4b84d09f20da1f5c937
Felipe <felipe.osorio72@outlook.com.br>
2017-04-09 18:44:42 -0300

    Um commit

--- src/Graph.hs
+++ src/Graph.hs
@@ -1,22 +1,16 @@
 infinity = 1e1000
 
-data Node = Node {
-     value :: Int
-     }
-            deriving(Show, Eq, Ord)
+type Node = Int
 data Vertex = Vertex {
      adjacency :: (Node, Node),
-     weight :: Int
-     
-     }
-            deriving(Show)
-data Graph = Graph [Vertex]
-            deriving(Show)
+     weight :: Int    
+     } deriving(Show)
+type Graph = [Vertex]
 
 createNodes 0 = []
-createNodes num = (Node num) : (createNodes (num - 1))
+createNodes num = (num) : (createNodes (num - 1))
 
-nodes = createNodes 5
+nodes = reverse(createNodes 5)
 
 vertexes = [Vertex (nodes !! 0, nodes !! 1) 1, Vertex ( nodes !! 1, nodes !! 0) 1,
 		 	Vertex (nodes !! 0, nodes !! 2) 1, Vertex (nodes !! 2, nodes !! 0) 1,
@@ -25,4 +19,15 @@ vertexes = [Vertex (nodes !! 0, nodes !! 1) 1, Vertex ( nodes !! 1, nodes !! 0) 
 			Vertex (nodes !! 4, nodes !! 3) 8, Vertex ( nodes !! 3, nodes !! 4) 8,
 			Vertex (nodes !! 2, nodes !! 3) 3, Vertex (nodes !! 3, nodes !! 2) 3]
 
-g = Graph vertexes
+foreach [] _ = []			
+foreach (x:xs) f = (f x) : (foreach xs f)
+
+g = map adjacency (vertexes)
+
+neighbors g n = filter (\(nd, _) -> nd == n) $ g
+
+weightInNode v n = weight . head . filter (\e -> n == fst(adjacency e)) $ v
+
+-- connectedNodes v = map fst(adjacency v) 
+
+dnodeForNode dnodes n = head . filter (\(x, _) -> x == n) $ dnodes
